090a880501cadb051170ae407ecd7475e7de6e8d
Ahmad Romadhon <romadhon1byar@gmail.com>
2020-11-06 01:25:45 +0700

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+date_default_timezone_set('Asia/Jakarta');
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
