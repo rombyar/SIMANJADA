0ed67bef2c3fb5c738b2545b6a1e1cf2e1628b6c
Ahmad Romadhon <romadhon1byar@gmail.com>
2020-11-06 02:00:38 +0700

    Update

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '12345';
+$db['default']['database'] = 'db_masjid';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
